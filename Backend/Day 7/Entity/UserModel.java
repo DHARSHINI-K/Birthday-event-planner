cf48a7cbd2ecd6327de7152f20a4df82383877a7
DHARSHINI-K <113882668+DHARSHINI-K@users.noreply.github.com>
2024-03-05 12:28:54 +0530

    Add files via upload

---
+++ Backend/Day 7/Entity/UserModel.java
@@ -0,0 +1,26 @@
+package blowout.blowout.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private long mobileNumber;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ Backend/Day 7/dto/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package blowout.blowout.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+    private String userRole;
+   
+}
