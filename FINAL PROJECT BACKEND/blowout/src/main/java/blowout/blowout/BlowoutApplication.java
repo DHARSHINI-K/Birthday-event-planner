8e07c53dfbbeb9c6c20e1bc038ba3d31b8393587
DHARSHINI-K <113882668+DHARSHINI-K@users.noreply.github.com>
2024-03-11 11:32:48 +0530

    Add files via upload

---
+++ FINAL PROJECT BACKEND/blowout/src/main/java/blowout/blowout/BlowoutApplication.java
@@ -0,0 +1,13 @@
+package blowout.blowout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlowoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlowoutApplication.class, args);
+	}
+
+}
---
+++ FINAL PROJECT BACKEND/blowout/src/main/java/blowout/blowout/Entity/Role.java
@@ -0,0 +1,5 @@
+package blowout.blowout.Entity;
+public enum Role {
+    USER,
+    ADMIN
+}
